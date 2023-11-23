552a96aca2e4c64084bec5e7e96297b10fe40382
JuhyeonLee1 <zugusdl@gmail.com>
2023-11-23 15:19:34 +0900

    ㅇ

--- trade/src/main/java/com/itwillbs/product/action/DeleteProductAction.java
+++ trade/src/main/java/com/itwillbs/product/action/DeleteProductAction.java
@@ -38,8 +38,8 @@ public class DeleteProductAction implements Action {
 			
 			for(String dFileName : dFile_name) {
 				// dFileName 파일 삭제 수행
-				file = new File(realPath + "\\" + dFileName);
-//				file = new File("/upload/"+dFileName);
+//				file = new File(realPath + "\\" + dFileName);
+				file = new File("/trade/upload"+"\\"+dFileName);
 				file.delete();
 			}
 		}
